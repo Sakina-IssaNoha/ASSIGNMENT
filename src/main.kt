cebb685fcf02fce39432842e14eff52e8256ae58
Sakina-IssaNoha <sakinnanoha@gmail.com>
2022-02-17 08:38:54 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var name = "Sakina Issa"
+    println(name)
+    var age = 21
+    println(age)
+    val phoneNumber: String = "074876900"
+    println(phoneNumber)
+var weight = (52)
+    println(weight)
+    var iskenyan : Boolean = true
+    println(iskenyan)
+
+
+}
+
+
